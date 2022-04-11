24cd4328fc9cb6ca7b25c3c5065feed2ef18cb70
oott123 <git@public.oott123.com>
2022-04-11 10:27:37 +0800

    chore: add provider

---
+++ app/Providers/NatsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NatsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('App\Service\NatsService', 'App\Service\NatsService');
+    }
+}
